818a41595b6f639c4a95310c94f06ade7db4a842
zilizhou <zlzhou999@163.com>
2018-01-31 08:37:16 +0800

    Initial commit

---
+++ paperflask.py
@@ -0,0 +1,32 @@
+## -*- coding: utf-8 -*-
+import paperidx
+from flask import Flask,request,render_template,redirect
+import json
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('paperfind.html')
+
+@app.route('/search',methods=['GET','POST'])
+def search():
+    result={}
+    if request.method == 'POST':
+        resu=[]
+        if request.form['sel']=='paper':
+            paper = request.form['qinfo'].strip()
+            resu=paperidx.searInfo(paper)
+            result['paper']=resu
+            result['journal']=[]
+        if request.form['sel']=='journal':
+            jour = request.form['qinfo'].strip()
+            resu=paperidx.searjInfo(jour)
+            result['paper'] = []
+            result['journal'] = resu
+        print(result)
+        # print(resu)
+        # journal = request.form['sel']
+        return json.dumps(result,ensure_ascii=False)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0',debug=True)
---
+++ paperidx.py
@@ -0,0 +1,64 @@
+## -*- coding: utf-8 -*-
+import pymongo
+import docx
+from pymongo import MongoClient
+
+# conn=MongoClient('localhost',27017)
+# db=conn['paperIdx']
+# paper=db['paper']
+# jour=db['jour']
+pinfolist=[]
+jinfolist=[]
+def openFile():
+    # r = filedialog.askopenfilename(title='打开文件', filetypes=[('docx', '*.docx'), ('All Files', '*')])
+    file = docx.Document('esi.docx')
+    for i in range(len(file.paragraphs)):
+        infos=file.paragraphs[i].text
+
+        if infos:
+            pinfolist.append(infos)
+
+    journalfile=open('journal.txt','r')
+    lines=journalfile.readlines()
+    for l in lines:
+        jinfolist.append(l)
+    # print(jinfolist)
+
+def openTableFile():
+    journallist=[]
+    file = docx.Document('journal.docx')
+    t=file.tables[0]
+    f=open('journal.txt','a')
+    # print(len(t.cols))
+
+    for j in range(1,len(t.rows)):
+        temp = []
+        for i in range(1,5):
+            temp.append(t.cell(j,i).text)
+
+        f.write(','.join(temp))
+        f.write('\n')
+        f.flush()
+    f.close()
+def searjInfo(str):
+    result = []
+    for info in jinfolist:
+
+        if str in info:
+            result.append(info)
+    return result
+def searInfo(str):
+
+    result=[]
+    for info in pinfolist:
+
+        if str in info:
+            result.append(info)
+    return result
+openFile()
+if __name__=='__main__':
+    pass
+    # openTableFile()
+    # info=searInfo('Some new oscillation results for linear Hamiltonian systems')
+    info=searjInfo('7.26')
+    print(info)
